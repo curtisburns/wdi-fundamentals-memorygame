9a2fea67e46f20bbf40ef266a1e5ecc8b4f7e9f8
Curtis Burns <thisismrburns@gmail.com>
2018-06-06 18:51:18 +0100

    Replaced card strings with objects

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,25 @@
-var cards = ["queen","queen","king","king"];
+var cards = [
+  {
+    rank: "queen",
+    suit: "hearts",
+    cardImage: "images/queen-of-hearts.png" ,
+  },
+  {
+    rank: "queen",
+    suit: "diamonds",
+    cardImage: "images/queen-of-diamonds.png",
+  },
+  {
+    rank: "king",
+    suit: "hearts",
+    cardImage: "images/king-of-hearts.png",
+  },
+  {
+    rank: "king",
+    suit: "diamonds",
+    cardImage: "images/king-of-diamonds.png"
+  }
+];
 var cardsInPlay = [];
 
 //Checks for matches in the cardsInPlay array. Must define before flipCard as we call this function later on.
@@ -14,11 +35,13 @@ var checkForMatch = function() {
 
 //Function for user flipping cards - adds selected cards to cardsInPlay array.
 var flipCard = function(cardId) {
-console.log(`User flipped ${cards[cardId]}`);
-  cardsInPlay.push(cards[cardId]);
+console.log(`User flipped ${cards[cardId].rank}`);
+  cardsInPlay.push(cards[cardId].rank);
+console.log(cards[cardId].cardImage);
+console.log(cards[cardId].suit);
   checkForMatch();
 };
 
 //Calling flipCard function to test result.
 flipCard(3);
-flipCard(2);
+flipCard(1);
